69bc6e74ef4c2158a57dd79425176b19ec2d4d55
sara.diaz.oporto@gmail.com <sara.diaz.oporto@gmail.com>
2019-04-25 22:43:51 -0500

    Nuevo repo

---
+++ Source/BananaOficial/BananaOficial.cpp
@@ -0,0 +1,7 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#include "BananaOficial.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, BananaOficial, "BananaOficial" );
+ 
